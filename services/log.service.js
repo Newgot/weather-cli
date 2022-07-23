bb43e3a50cd04831b319c8400a3789e8523f6e59
Vladislav Bakhov <newgot@mail.ru>
2022-07-23 22:26:10 +0700

    Создание каркаса приложения

---
+++ services/log.service.js
@@ -0,0 +1,19 @@
+import chalk from 'chalk'
+import dedent from 'dedent-js';
+
+export const printError = (err) => {
+    console.log(chalk.bgRed(' ERROR ') + ' ' + err);
+}
+
+export const printSucces = (msg) => {
+    console.log(chalk.bgGreen(' ERROR ') + ' ' + err);
+}
+export const printHelp = () => {
+    console.log(
+        dedent `${chalk.bgCyan(' HELP ')}
+        Бех параметров - вывод погоды
+        -s [CITY] для установки города
+        -h для вывода помощи
+        -t [APY_KEY] для cохранения токена
+    `);
+}
